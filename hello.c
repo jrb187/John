68f947d8d564472570437dcb0f82a80afd9afde9
jrb187 <42649900+jrb187@users.noreply.github.com>
2018-08-23 11:41:47 -0500

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Seongjune Han, Cade Rasmussen
+ * Date: 2018/08/21
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Seongjune Han... Mathematics and Computer Science\n");
+
+  return 0;
+}
